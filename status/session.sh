7afa8ed5abb62dd7f1e835433922c0da015ffc13
Alan Morey <morey.alan@gmail.com>
2024-01-28 11:33:00 -0800

    Change session icon when there is client prefix

--- status/session.sh
+++ status/session.sh
@@ -1,6 +1,25 @@
 show_session() {
   local index=$1
-  local icon=$(get_tmux_option "@catppuccin_session_icon" "")
+
+  # Nerd font terminal icons
+  #
+  #   - nf-cod-terminal
+  #   - nf-dev-terminal
+  #   - nf-fa-terminal
+  #   - nf-oct-terminal
+  #   - nf-cod-terminal_powershell
+  #   - nf-dev-terminal_badge
+  #   - nf-cod-terminal_tmux
+
+  # Swap session icon based on prefix
+  #
+  local icon=$(get_tmux_option "@catppuccin_session_icon" "#{?client_prefix,,}")
+  #local icon=$(get_tmux_option "@catppuccin_session_icon" "#{?client_prefix,,}")
+  #local icon=$(get_tmux_option "@catppuccin_session_icon" "#{?client_prefix,,}")
+  #local icon=$(get_tmux_option "@catppuccin_session_icon" "#{?client_prefix,,}")
+  #local icon=$(get_tmux_option "@catppuccin_session_icon" "#{?client_prefix,,}")
+  #local icon=$(get_tmux_option "@catppuccin_session_icon" "#{?client_prefix,,}")
+
   local color=$(get_tmux_option "@catppuccin_session_color" "#{?client_prefix,$thm_red,$thm_green}")
   local text=$(get_tmux_option "@catppuccin_session_text" "#S")
 
